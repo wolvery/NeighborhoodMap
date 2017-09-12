298303a2a343a10726efb7484c33fbcf69c085e4
Guilherme Goncalves <guilherme.silva@remopt.com>
2017-09-11 21:46:59 -0300

    Fix previous erros to api.js

--- js/api.js
+++ js/api.js
@@ -1,23 +1,42 @@
+function toggleBounce(marker) {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
+function getClimateAtBH() {
+    $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/weather?q=belo%20horizonte,br&appid=41ed607378c57096980252995587b30d'
+    }).done(function (result) {
+        document.getElementById("climate").append("BH's weather has " + result.weather["0"].description);
+    }).fail(function () {
+        alert("Could not find out the proper weather to BH. Sorry :/");
+    });
+}
+
+
 var infoContent = '<div id="content">' +
     '<div >' +
     '<h3>$TITLE$</h3>' +
     '</div>' +
     '<br>' +
     '<div id="bodyContent">' +
-    '$status$' +  
+    '$status$' +
     '<br>' +
-    '$open_hours$' +    
+    '$open_hours$' +
     '<br>' +
-    '$reviews$' + 
+    '$reviews$' +
     '<br>' +
-    '$address$' + 
+    '$address$' +
     '</div>' +
     '</div>';
 var greenIcon = 'https://chart.googleapis.com/chart?chst=d_map_pin_icon&chld=snack|008000';
 var whiteIcon = 'https://chart.googleapis.com/chart?chst=d_map_pin_icon&chld=repair|FFFFFF';
 
 
-function initMap() {
+var initMap = function() {
     getClimateAtBH();
     //https://review.udacity.com/#!/rubrics/17/view
     var viewModel = {
@@ -79,6 +98,69 @@ function initMap() {
     var service = new google.maps.places.PlacesService(map);
     var largeInfowindow = new google.maps.InfoWindow();
     var bounds = new google.maps.LatLngBounds();
+    var populateInfoWindow = function (marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+            infowindow.marker = marker;
+
+            var request = {
+                placeId: marker.place_id
+            };
+            service.getDetails(request, function callback(result, status) {
+                var tempInfoContent = infoContent.replace("$TITLE$", marker.title);
+                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                    if (result.opening_hours.open_now) {
+                        tempInfoContent = tempInfoContent.replace("$status$", "It is open, now! :)");
+                        marker.setIcon(greenIcon);
+                    } else {
+                        tempInfoContent = tempInfoContent.replace("$status$", "It is closed, now! :/");
+                        marker.setIcon(whiteIcon);
+                    }
+
+                    if (result.rating) {
+                        tempInfoContent = tempInfoContent.replace("$reviews$", "Stars:" + result.rating);
+                    } else {
+                        tempInfoContent = tempInfoContent.replace("$reviews$", "");
+                    }
+
+                    if (result.formatted_address) {
+                        tempInfoContent = tempInfoContent.replace("$address$", result.formatted_address);
+                    } else {
+                        tempInfoContent = tempInfoContent.replace("$address$", "");
+
+                    }
+
+                    if (result.opening_hours.weekday_text) {
+                        var today = new Date().getDay() - 1;
+                        today = (today < 0) ? 6 : today;
+
+                        tempInfoContent = tempInfoContent.replace("$open_hours$", result.opening_hours.weekday_text[today]);
+                    } else {
+                        tempInfoContent = tempInfoContent.replace("$open_hours$", "");
+                    }
+                } else {
+                    tempInfoContent = tempInfoContent.replace("$status$", "We do not have more info about this place. :/");
+                    tempInfoContent = tempInfoContent.replace("$address$", "But, it is good. Trust me.");
+                    tempInfoContent = tempInfoContent.replace("$open_hours$", "");
+                    tempInfoContent = tempInfoContent.replace("$reviews$", "");
+
+                }
+                infowindow.setContent(tempInfoContent);
+                infowindow.open(map, marker);
+                document.getElementById("pizzaElement").value = marker.title;
+            });
+
+
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick', function () {
+                infowindow.setMarker = null;
+            });        
+
+        }
+    };
+
+
+
     var i = 0;
     viewModel.locations.forEach(function (element) {
         var marker = new google.maps.Marker({
@@ -101,10 +183,7 @@ function initMap() {
     map.fitBounds(bounds);
 
     var selector = document.getElementById("pizzaElement");
-    selector.addEventListener("change", focusOnMarker);
-
-
-    function focusOnMarker() {
+    selector.addEventListener("change", function focusOnMarker() {
         var x = document.getElementById("pizzaElement").value;
         var boundsFocus = new google.maps.LatLngBounds();
 
@@ -112,7 +191,7 @@ function initMap() {
         for (var i = 0; i < markerList.length; i++) {
             if (x === "All pizzeria" || x === "Select here...") {
                 largeInfowindow.close();
-                if (markerList[i].map == null) {
+                if (markerList[i].map === null) {
                     markerList[i].setMap(map);
                 }
                 markerList[i].setAnimation(null);
@@ -121,7 +200,7 @@ function initMap() {
             }
             else {
                 if (x === (markerList[i].title)) {
-                    if (markerList[i].map == null) {
+                    if (markerList[i].map === null) {
                         markerList[i].setMap(map);
                     }
 
@@ -141,92 +220,14 @@ function initMap() {
 
         // if none has been selected, reloads the bound and show them all
         map.fitBounds(boundsFocus);
-    }
+    });
 
 
 
-function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-        infowindow.marker = marker;
-        
-        var request = {
-            placeId: marker.place_id
-        };
-        service.getDetails(request, callback);
-
-        
-        // Make sure the marker property is cleared if the infowindow is closed.
-        infowindow.addListener('closeclick', function () {
-            infowindow.setMarker = null;
-        });
-
-        function callback(result, status) {
-            var tempInfoContent = infoContent.replace("$TITLE$", marker.title);
-            if (status == google.maps.places.PlacesServiceStatus.OK) {
-                if (result.opening_hours.open_now) {
-                    tempInfoContent =  tempInfoContent.replace("$status$", "It is open, now! :)");            
-                    marker.setIcon(greenIcon);
-                } else {
-                    tempInfoContent =  tempInfoContent.replace("$status$", "It is closed, now! :/");                        
-                    marker.setIcon(whiteIcon);
-                }
-
-                if (result.rating) {
-                    tempInfoContent =  tempInfoContent.replace("$reviews$","Stars:" + result.rating);
-                } else{
-                    tempInfoContent =  tempInfoContent.replace("$reviews$", "");                    
-                }
-        
-                if (result.formatted_address) {
-                    tempInfoContent =  tempInfoContent.replace("$address$", result.formatted_address);
-                } else{
-                    tempInfoContent =  tempInfoContent.replace("$address$", "");
-                    
-                }               
-        
-                if (result.opening_hours.weekday_text){
-                    var today =  new Date().getDay() - 1;
-                    today = (today < 0) ? 6 : today;    
-                    
-                    tempInfoContent =  tempInfoContent.replace("$open_hours$",result.opening_hours.weekday_text[today]);        
-                }else{
-                    tempInfoContent =  tempInfoContent.replace("$open_hours$", "");                    
-                }
-            } else {
-                tempInfoContent =  tempInfoContent.replace("$status$", "We do not have more info about this place. :/");                        
-                tempInfoContent =  tempInfoContent.replace("$address$", "But, it is good. Trust me.");
-                tempInfoContent =  tempInfoContent.replace("$open_hours$", "");                          
-                tempInfoContent =  tempInfoContent.replace("$reviews$", "");                    
-                
-            }
-            infowindow.setContent(tempInfoContent);
-            infowindow.open(map, marker);
-            document.getElementById("pizzaElement").value = marker.title;
-        }
-
-
-
-
-    }
-};
-}
-function toggleBounce(marker) {
-    if (marker.getAnimation() !== null) {
-        marker.setAnimation(null);
-    } else {
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
+    
 }
 
-function getClimateAtBH() {
-    $.ajax({
-        url: 'https://api.openweathermap.org/data/2.5/weather?q=belo%20horizonte,br&appid=41ed607378c57096980252995587b30d'
-    }).done(function (result) {
-        document.getElementById("climate").append("BH's weather has " + result.weather["0"].description);
-    }).fail(function () {
-        alert("Could not find out the proper weather to BH. Sorry :/");
-    })
-}
+
+
 
 
